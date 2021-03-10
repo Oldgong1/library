459f979ec33c0cf613f8c5a78fca2b5a7c4492e9
lunkurli <lunkurli@gmail.com>
2021-03-11 01:18:45 +0800

    updated

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -25,10 +25,12 @@ from django.views.generic import RedirectView
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
+#Add Django site authentication urls (for login, logout, password management)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
