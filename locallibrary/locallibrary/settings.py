a4743df7d03164d9021a3ed24ea4a4b9534cd72c
lunkurli <lunkurli@gmail.com>
2021-03-11 16:26:15 +0800

    changes made

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'a^t7kv$e*ygqku8tf^9^1#21fn3s7v&vu1gsmgliv@&i5#e0#u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
